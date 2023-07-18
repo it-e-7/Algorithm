315f755b1e50e25ff614695898568df9a4fe4056
tigre911 <94420939+tigre911@users.noreply.github.com>
2023-07-18 18:23:41 +0900

    Update 기능개발_김진우.py

--- Programmers/스택_큐/기능개발_김진우.py
+++ Programmers/스택_큐/기능개발_김진우.py
@@ -10,17 +10,11 @@ def solution(progresses, speeds):
     while progresses:
         for i in range(len(progresses)):
             progresses[i] += speeds[i]
-            print(f'progresses, speeds : {progresses, speeds}')
         count = 0
-        print(f"***progresses and progresses[0] : {progresses , progresses[0]}")
         while progresses and progresses[0] >= 100:
-            # progresses 리스트가 비어있지 않은동안은 계속 반복 + 가장 앞에 있는 기능이 100% 이상일 경우 배포
             progresses.popleft()
             speeds.popleft()
-            print("while문")
-            print(f'progresses : {progresses} /// speeds : {speeds}')
             count += 1
-            print(count)
         if count > 0:
             answer.append(count)
 
